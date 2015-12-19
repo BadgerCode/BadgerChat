54c737396ead6b9a897730b2514e30e7ebaa510e
Badger <badger@badgercode.co.uk>
2015-12-19 20:28:07 +0000

    Adds basic install and is_installed

--- upload/inc/plugins/badgerchat.php
+++ upload/inc/plugins/badgerchat.php
@@ -8,7 +8,7 @@ if(!defined("IN_MYBB")){
 function badgerchat_info(){
     return array(
         "name"          => "BadgerChat",
-        "Description"   => "A chat box plugin",
+        "description"   => "A chat box plugin",
         "website"       => "https://github.com/BadgerCode/BadgerChat",
         "author"        => "Badger",
         "authorsite"    => "http://badgercode.co.uk",
@@ -19,14 +19,22 @@ function badgerchat_info(){
 }
 
 function badgerchat_install(){
-    
+    // TODO: Proper migration management
+    global $db;
+    $migrationQueries = explode(";", file_get_contents(MYBB_ROOT . "inc/plugins/badgerchat/migrations/1.sql"));
+    foreach($migrationQueries AS $migrationQuery) {
+        $trimmedQuery = trim($migrationQuery);
+        if(empty($trimmedQuery)){
+            continue;
+        }
+
+        $db->write_query($trimmedQuery);
+    }
 }
 
 function badgerchat_is_installed(){
-    /*
-     * global $db;
-     * return $db->table_exists("tableName");
-     */
+    // TODO: Proper migration management
+    return badgerchat_GetCurrentDBVersion() == 1;
 }
 
 function badgerchat_uninstall(){
@@ -39,4 +47,17 @@ function badgerchat_activate(){
 
 function badgerchat_deactivate(){
 
+}
+
+function badgerchat_GetCurrentDBVersion(){
+    global $db;
+
+    $query = $db->simple_select("badgerchat_version",
+        "Version",
+        "",
+        array("order_by" => "InstalledAt", "order_dir" => "DESC", "limit" => "1"));
+
+    $currentVersion = $db->fetch_field($query, "Version");
+
+    return is_numeric($currentVersion) ? intval($currentVersion) : -1;
 }
